7d2048fa806d1ff5f303a72f819c0ca81e8ab85d
Lenovo <Lenovo@Rayudu>
2020-10-10 23:36:58 +0530

    Test Commit

---
+++ ProjectSriDevi/src/main/java/com/naren/snr/ProjectSriDeviApplication.java
@@ -0,0 +1,14 @@
+package com.naren.snr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ProjectSriDeviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSriDeviApplication.class, args);
+	}
+
+}
---
+++ ProjectSriDevi/src/main/java/com/naren/snr/PublicUser.java
@@ -0,0 +1,37 @@
+package com.naren.snr;
+
+public class PublicUser {
+	private String username;
+	private String password;
+	private String email;
+	private long mobile_number;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getMobile_number() {
+		return mobile_number;
+	}
+	public void setMobile_number(long mobile_number) {
+		this.mobile_number = mobile_number;
+	}
+	
+	
+	
+}
